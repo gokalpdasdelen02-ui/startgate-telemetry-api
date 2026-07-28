6b55baad2f1a5ab7beef5bbc5202cbd26faefd62
deneme <deneme>
2026-07-28 16:58:12 +0300

    style: format application code

--- app/database.py
+++ app/database.py
@@ -2,19 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#veritabanındaki dosyaların yerini belirliyoruz.  
+# veritabanındaki dosyaların yerini belirliyoruz.
 SQLALCHEMY_DATABASE_URL = "sqlite:///./telemetry.db"
 
-#motoru oluşturuyoruz.  
+# motoru oluşturuyoruz.
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-#veritabanı işlemleri için oturum oluşturuyoruz.
+# veritabanı işlemleri için oturum oluşturuyoruz.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#model sınıflarımızın temelini oluşturuyoruz.
+# model sınıflarımızın temelini oluşturuyoruz.
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
--- app/main.py
+++ app/main.py
@@ -5,14 +5,15 @@ from app.routers import events
 
 app = FastAPI(title="Game Telemetry API")
 
-models.Base.metadata.create_all(bind=engine) #modellerden veri tabanı oluşturmamızı sağlayn kod.
+models.Base.metadata.create_all(
+    bind=engine
+)  # modellerden veri tabanı oluşturmamızı sağlayn kod.
 
-app.include_router(events.router) #events.py dosyasındaki router'ı ana uygulamaya ekliyoruz.
+app.include_router(
+    events.router
+)  # events.py dosyasındaki router'ı ana uygulamaya ekliyoruz.
+
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 def health_check():
-    return{
-        "status": "ok", 
-        "message": "Service is healthy."
-
-    }
+    return {"status": "ok", "message": "Service is healthy."}
